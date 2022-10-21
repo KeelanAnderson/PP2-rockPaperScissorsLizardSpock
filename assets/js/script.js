0d7e783f224444c82fe2ebaa32713e1f60a15ba0
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-10-21 09:52:22 +0000

    grab elements from the dom in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const playerChoiceDisplay = document.getElementById('player-choice')
+const resultDisplay = document.getElementById('result')
+const gameButtons = document.querySelectorAll('button')
