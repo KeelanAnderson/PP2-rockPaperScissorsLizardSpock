60915016d572ae07244be16d30156a351a9e888f
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-10-21 10:22:20 +0000

    add function to display players choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const playerChoiceDisplay = document.getElementById('player-choice')
-const resultDisplay = document.getElementById('result')
-const gameButtons = document.querySelectorAll('button')
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const resultDisplay = document.getElementById('result');
+const gameButtons = document.querySelectorAll('button');
+let userChoice;
+
+gameButtons.forEach(gameButton => gameButton.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    playerChoiceDisplay.innerHTML = userChoice 
+    console.log(gameButton)
+    generateComputerChoice()
+    
+}))
+
+function generateComputerChoice() {
+
+}
