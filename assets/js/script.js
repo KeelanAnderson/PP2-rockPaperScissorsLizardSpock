7970d8363d8a27edbad7909bcdaba2ee4d8d1624
KeelanAnderson <keelan.anderson.ka@gmail.com>
2022-10-29 18:27:45 +0000

    add data structure and  get result function to show the winner

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,73 +2,50 @@ const computerChoiceDisplay = document.getElementById('computer-choice');
 const playerChoiceDisplay = document.getElementById('player-choice');
 const resultDisplay = document.getElementById('result');
 const gameButtons = document.querySelectorAll('[data-selection]');
-const computerChoice = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+let computerChoiceOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 let userChoice;
+let computerChoice;
 let result;
-let selection = document.getElementsByClassName('selection')
-
-function openGame() {
-    window.location.href = "game.html"
-}
 
 gameButtons.forEach(gameButton => gameButton.addEventListener('click', (e) => {
     userChoice = e.target.dataset;
-    playerChoiceDisplay.innerHTML = userChoice;
     playerChoiceDisplay.setAttribute("src", `assets/images/${e.target.id}-image.png`); // sets the images for players choice
-    generateComputerChoice();
     getResult();
-    console.log(userChoice)
-    
-
 }));
 
 function generateComputerChoice() {
-    randomNumber = Math.floor(Math.random() * computerChoice.length);
-    computerChoiceDisplay.innerHTML = computerChoice[randomNumber]
-    computerChoiceDisplay.setAttribute("src", `assets/images/${computerChoice[randomNumber]}-image.png`); // sets the image for computer choice
-    computerChoiceDisplay.setAttribute("alt", `${computerChoice[randomNumber]}`); //sets alt text for image for computer choice
+    let randomNumber = Math.floor(Math.random() * 5);
+    computerChoiceDisplay.innerHTML = computerChoiceOptions[randomNumber];
+    computerChoiceDisplay.setAttribute("src", `assets/images/${computerChoiceOptions[randomNumber]}-image.png`); // sets the image for computer choice
+    computerChoiceDisplay.setAttribute("alt", `${computerChoiceOptions[randomNumber]}`); //sets alt text for image for computer choice
+    computerChoice = computerChoiceOptions[randomNumber];
+
+
 }
 
-// get results
+const options = {
+    'rock': ['scissors', 'lizard'],
+    'paper': ['rock', 'spock'],
+    'scissors': ['paper', 'lizard'],
+    'lizard': ['paper', 'spock'],
+    'spock': ['scissors', 'rock'],
+};
 
-const options = [
-    {
-        name: 'rock',
-        img: 'rock-image.png',
-        beats: ['lizard', 'scissors']
-    },
-    {
-        name: 'paper',
-        img: 'paper-image.png',
-        beats: ['rock', 'spock']
-    },
-    {
-        name: 'scissors',
-        img: 'scissors-image.png',
-        beats: ['lizard', 'paper']
-    },
-    {
-        name: 'lizard',
-        img: 'lizard-image.png',
-        beats: ['paper', 'spock']
-    },
-    {
-        name: 'spock',
-        img: 'spock-image.png',
-        beats: ['rock', 'scissors']
-    },
-
-]
-
-function getResult(userChoice, computerChoice) {
-    userChoice = gameButtons
-    if (userChoice === computerChoice) {
-        result = 'its a draw'
+function getResult() {
+    generateComputerChoice();
+    beatenArray = options[userChoice.selection];
+    let answer = beatenArray.includes(computerChoice);
+    if (answer == true) {
+        result = 'You Win';
+        incrementPlayerScore();
+    } else if (answer == false) {
+        result = 'You Lose';
+        incrementComputerScore();
     }
-    userChoice = options.find(() => options.name === computerChoice)
-    console.log(userChoice)
-    if()
-
+    else (computerChoice === userChoice.selection) {
+        result = "its a draw";
+    }
+    resultDisplay.innerHTML = result;
 }
 
 //increment score for computer and player
@@ -82,9 +59,3 @@ function incrementComputerScore() {
     let computerScore = parseInt(document.getElementById('computer-score').innerText);
     document.getElementById('computer-score').innerText = ++computerScore;
 }
-
-function endGame() {
-    window.location.href = "gameover.html"
-}
-
-// game mode functions
--- dump.js
+++ dump.js
@@ -157,3 +157,113 @@ winnersAndLosers = {
 }
 playerChoiceDisplay.innerHTML = userChoice;
 playerChoiceDisplay.setAttribute("src", `assets/images/${e.target.id}-image.png`); // sets the images for players choice
+
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const resultDisplay = document.getElementById('result');
+const gameButtons = document.querySelectorAll('[data-selection]');
+const computerChoice = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+let userChoice;
+let result;
+let selection = document.getElementsByClassName('selection')
+let randomNumber
+
+let computerPick = computerChoiceDisplay.innerHTML
+console.log(computerPick)
+
+function openGame() {
+    window.location.href = "game.html"
+}
+
+gameButtons.forEach(gameButton => gameButton.addEventListener('click', (e) => {
+    userChoice = e.target.dataset;
+    playerChoiceDisplay.innerHTML = userChoice;
+    playerChoiceDisplay.setAttribute("src", `assets/images/${e.target.id}-image.png`); // sets the images for players choice
+    generateComputerChoice();
+    getResult();
+    console.log(userChoice)
+    
+
+}));
+
+function generateComputerChoice(randomNumber) {
+    randomNumber = Math.floor(Math.random() * 5);
+    computerChoiceDisplay.innerHTML = computerChoice[randomNumber]
+    computerChoiceDisplay.setAttribute("src", `assets/images/${computerChoice[randomNumber]}-image.png`); // sets the image for computer choice
+    computerChoiceDisplay.setAttribute("alt", `${computerChoice[randomNumber]}`); //sets alt text for image for computer choice
+    console.log(computerChoice[randomNumber])
+}
+console.log(computerChoice[randomNumber])
+
+// get results
+
+const options = [
+    {
+        name: 'rock',
+        img: 'rock-image.png',
+        beats: ['lizard', 'scissors']
+    },
+    {
+        name: 'paper',
+        img: 'paper-image.png',
+        beats: ['rock', 'spock']
+    },
+    {
+        name: 'scissors',
+        img: 'scissors-image.png',
+        beats: ['lizard', 'paper']
+    },
+    {
+        name: 'lizard',
+        img: 'lizard-image.png',
+        beats: ['paper', 'spock']
+    },
+    {
+        name: 'spock',
+        img: 'spock-image.png',
+        beats: ['rock', 'scissors']
+    },
+
+]
+computerChoice = generateComputerChoice(randomNumber)
+console.log(options[computerChoice].beats)
+
+
+function getResult(userChoice, computerChoice) {
+    userChoice = gameButtons
+    if (userChoice === computerChoice) {
+        result = 'its a draw'
+    }
+    userChoice = options.find(() => options.name === computerPick)
+    options.generateComputerChoice(randomNumber) = computerChoice
+    console.log(computerChoice)
+    if(losersArray.indexOf()){
+       
+        return 'player wins'
+    }
+    else{
+        return 'computer wins'
+    }
+        
+
+    
+
+}
+
+//increment score for computer and player
+
+function incrementPlayerScore() {
+    let playerScore = parseInt(document.getElementById('player-score').innerText);
+    document.getElementById('player-score').innerText = ++playerScore;
+}
+
+function incrementComputerScore() {
+    let computerScore = parseInt(document.getElementById('computer-score').innerText);
+    document.getElementById('computer-score').innerText = ++computerScore;
+}
+
+function endGame() {
+    window.location.href = "gameover.html"
+}
+
+// game mode functions
